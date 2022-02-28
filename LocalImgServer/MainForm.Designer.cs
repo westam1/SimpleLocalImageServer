fe81130ee8ed9d074def42905828c347d22eb279
westam1 <93354999+westam1@users.noreply.github.com>
2022-02-27 20:52:02 -0500

    Mostly works, but not in all cases

--- LocalImgServer/MainForm.Designer.cs
+++ LocalImgServer/MainForm.Designer.cs
@@ -24,13 +24,235 @@ namespace LocalImgServer {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.TlpMain = new System.Windows.Forms.TableLayoutPanel();
+			this.GbxConfig = new System.Windows.Forms.GroupBox();
+			this.TlpConfig = new System.Windows.Forms.TableLayoutPanel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.TbxAddr = new System.Windows.Forms.TextBox();
+			this.TbxPort = new System.Windows.Forms.TextBox();
+			this.TbxDomain = new System.Windows.Forms.TextBox();
+			this.BtnRun = new System.Windows.Forms.Button();
+			this.LvwFiles = new System.Windows.Forms.ListView();
+			this.CmhName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.TbxLog = new System.Windows.Forms.TextBox();
+			this.LblServerPrefix = new System.Windows.Forms.TextBox();
+			this.TlpMain.SuspendLayout();
+			this.GbxConfig.SuspendLayout();
+			this.TlpConfig.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// TlpMain
+			// 
+			this.TlpMain.ColumnCount = 2;
+			this.TlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.TlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.TlpMain.Controls.Add(this.GbxConfig, 0, 0);
+			this.TlpMain.Controls.Add(this.LvwFiles, 1, 0);
+			this.TlpMain.Controls.Add(this.TbxLog, 0, 1);
+			this.TlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.TlpMain.Location = new System.Drawing.Point(0, 0);
+			this.TlpMain.Name = "TlpMain";
+			this.TlpMain.RowCount = 2;
+			this.TlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 140F));
+			this.TlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.TlpMain.Size = new System.Drawing.Size(628, 325);
+			this.TlpMain.TabIndex = 0;
+			// 
+			// GbxConfig
+			// 
+			this.GbxConfig.Controls.Add(this.TlpConfig);
+			this.GbxConfig.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.GbxConfig.Location = new System.Drawing.Point(3, 3);
+			this.GbxConfig.Name = "GbxConfig";
+			this.GbxConfig.Size = new System.Drawing.Size(308, 134);
+			this.GbxConfig.TabIndex = 0;
+			this.GbxConfig.TabStop = false;
+			this.GbxConfig.Text = "Server Config";
+			// 
+			// TlpConfig
+			// 
+			this.TlpConfig.ColumnCount = 2;
+			this.TlpConfig.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.TlpConfig.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.TlpConfig.Controls.Add(this.label1, 0, 0);
+			this.TlpConfig.Controls.Add(this.label2, 0, 1);
+			this.TlpConfig.Controls.Add(this.label3, 0, 2);
+			this.TlpConfig.Controls.Add(this.TbxAddr, 1, 0);
+			this.TlpConfig.Controls.Add(this.TbxPort, 1, 1);
+			this.TlpConfig.Controls.Add(this.TbxDomain, 1, 2);
+			this.TlpConfig.Controls.Add(this.BtnRun, 0, 4);
+			this.TlpConfig.Controls.Add(this.LblServerPrefix, 0, 3);
+			this.TlpConfig.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.TlpConfig.Location = new System.Drawing.Point(3, 16);
+			this.TlpConfig.Margin = new System.Windows.Forms.Padding(0);
+			this.TlpConfig.Name = "TlpConfig";
+			this.TlpConfig.RowCount = 5;
+			this.TlpConfig.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.TlpConfig.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.TlpConfig.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.TlpConfig.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.TlpConfig.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.TlpConfig.Size = new System.Drawing.Size(302, 115);
+			this.TlpConfig.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Location = new System.Drawing.Point(3, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(145, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Network Address:";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label2.Location = new System.Drawing.Point(3, 23);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(145, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Port";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label3.Location = new System.Drawing.Point(3, 46);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(145, 23);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "Domain";
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// TbxAddr
+			// 
+			this.TbxAddr.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.TbxAddr.Location = new System.Drawing.Point(154, 3);
+			this.TbxAddr.Name = "TbxAddr";
+			this.TbxAddr.Size = new System.Drawing.Size(145, 20);
+			this.TbxAddr.TabIndex = 4;
+			this.TbxAddr.Text = "127.0.0.1";
+			this.TbxAddr.TextChanged += new System.EventHandler(this.ServerParam_TextChanged);
+			// 
+			// TbxPort
+			// 
+			this.TbxPort.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.TbxPort.Location = new System.Drawing.Point(154, 26);
+			this.TbxPort.Name = "TbxPort";
+			this.TbxPort.Size = new System.Drawing.Size(145, 20);
+			this.TbxPort.TabIndex = 5;
+			this.TbxPort.Text = "8080";
+			this.TbxPort.TextChanged += new System.EventHandler(this.ServerParam_TextChanged);
+			// 
+			// TbxDomain
+			// 
+			this.TbxDomain.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.TbxDomain.Location = new System.Drawing.Point(154, 49);
+			this.TbxDomain.Name = "TbxDomain";
+			this.TbxDomain.Size = new System.Drawing.Size(145, 20);
+			this.TbxDomain.TabIndex = 6;
+			this.TbxDomain.Text = "ImgSvr";
+			this.TbxDomain.TextChanged += new System.EventHandler(this.ServerParam_TextChanged);
+			// 
+			// BtnRun
+			// 
+			this.BtnRun.BackColor = System.Drawing.Color.LightGreen;
+			this.TlpConfig.SetColumnSpan(this.BtnRun, 2);
+			this.BtnRun.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.BtnRun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.BtnRun.Location = new System.Drawing.Point(3, 92);
+			this.BtnRun.Margin = new System.Windows.Forms.Padding(3, 0, 3, 0);
+			this.BtnRun.Name = "BtnRun";
+			this.BtnRun.Size = new System.Drawing.Size(296, 23);
+			this.BtnRun.TabIndex = 7;
+			this.BtnRun.Text = "Run Server";
+			this.BtnRun.UseVisualStyleBackColor = false;
+			this.BtnRun.Click += new System.EventHandler(this.BtnRun_Click);
+			// 
+			// LvwFiles
+			// 
+			this.LvwFiles.BackColor = System.Drawing.SystemColors.Control;
+			this.LvwFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.CmhName});
+			this.LvwFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.LvwFiles.FullRowSelect = true;
+			this.LvwFiles.GridLines = true;
+			this.LvwFiles.HideSelection = false;
+			this.LvwFiles.Location = new System.Drawing.Point(317, 3);
+			this.LvwFiles.Name = "LvwFiles";
+			this.TlpMain.SetRowSpan(this.LvwFiles, 2);
+			this.LvwFiles.Size = new System.Drawing.Size(308, 319);
+			this.LvwFiles.TabIndex = 1;
+			this.LvwFiles.UseCompatibleStateImageBehavior = false;
+			this.LvwFiles.View = System.Windows.Forms.View.Details;
+			// 
+			// CmhName
+			// 
+			this.CmhName.Text = "Image Files";
+			this.CmhName.Width = 270;
+			// 
+			// TbxLog
+			// 
+			this.TbxLog.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.TbxLog.Location = new System.Drawing.Point(3, 143);
+			this.TbxLog.Multiline = true;
+			this.TbxLog.Name = "TbxLog";
+			this.TbxLog.ReadOnly = true;
+			this.TbxLog.Size = new System.Drawing.Size(308, 179);
+			this.TbxLog.TabIndex = 2;
+			// 
+			// LblServerPrefix
+			// 
+			this.LblServerPrefix.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.TlpConfig.SetColumnSpan(this.LblServerPrefix, 2);
+			this.LblServerPrefix.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.LblServerPrefix.Location = new System.Drawing.Point(3, 72);
+			this.LblServerPrefix.Name = "LblServerPrefix";
+			this.LblServerPrefix.ReadOnly = true;
+			this.LblServerPrefix.Size = new System.Drawing.Size(296, 13);
+			this.LblServerPrefix.TabIndex = 8;
+			this.LblServerPrefix.Text = "http://127.0.0.1:8080/ImgSvr/";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(628, 325);
+			this.Controls.Add(this.TlpMain);
+			this.Name = "MainForm";
+			this.Text = "Simple Local Image Server";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+			this.TlpMain.ResumeLayout(false);
+			this.TlpMain.PerformLayout();
+			this.GbxConfig.ResumeLayout(false);
+			this.TlpConfig.ResumeLayout(false);
+			this.TlpConfig.PerformLayout();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel TlpMain;
+		private System.Windows.Forms.GroupBox GbxConfig;
+		private System.Windows.Forms.TableLayoutPanel TlpConfig;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox TbxAddr;
+		private System.Windows.Forms.TextBox TbxPort;
+		private System.Windows.Forms.TextBox TbxDomain;
+		private System.Windows.Forms.Button BtnRun;
+		private System.Windows.Forms.ListView LvwFiles;
+		private System.Windows.Forms.TextBox TbxLog;
+		private System.Windows.Forms.ColumnHeader CmhName;
+		private System.Windows.Forms.TextBox LblServerPrefix;
 	}
 }
 
--- LocalImgServer/MainForm.cs
+++ LocalImgServer/MainForm.cs
@@ -1,17 +1,218 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.IO;
+using System.Net;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.ComponentModel;
+using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace LocalImgServer {
+	/// <summary> Main Form Software class. For now, all code will run from here. </summary>
 	public partial class MainForm : Form {
+		private const int LOG_MAX = 20;
+
+		private HttpListener m_Svr;
+		private BinaryWriter m_Log;
+		private string m_ImgDir;
+		private int m_DomainSize = 0;
+		private readonly Queue<string> m_LBack = new Queue<string>();
+		private readonly List<string> m_Images = new List<string>();
+
+		/// <summary> Construct a new form </summary>
 		public MainForm() {
 			InitializeComponent();
+
+			// Set up bare bones logging.
+			m_Log = new BinaryWriter(File.Open("log.txt", FileMode.OpenOrCreate));
+			m_Log.BaseStream.Position = 0;
+
+			Log("Application started");
+
+			// Set up and validate images directory
+			m_ImgDir = Application.StartupPath + "\\" + "Images";
+			if (!Directory.Exists(m_ImgDir)) {
+				Directory.CreateDirectory(m_ImgDir);
+			}
+
+			// Check to make sure we can actually do our thing
+			if (!HttpListener.IsSupported) {	// From MS Documentation
+				Log("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+				BtnRun.Enabled = false;
+				BtnRun.BackColor = Color.Red;
+			}
+
+			// Pre-populate DNS local name
+			TbxAddr.Text = Dns.GetHostName();
+
+			// Last, display files
+			DisplayFiles();
+		}
+		/// <summary> When the Server form is closing, perform proper cleanup. </summary>
+		private void MainForm_FormClosing(object sender, FormClosingEventArgs e) {
+			if (m_Log != null) { m_Log.Close(); }
+			if (m_Svr != null) { m_Svr.Stop(); }
+		}
+
+		/// <summary> Callback for incoming calls. Does the heavy lifting. </summary>
+		/// <param name="result"> Async parameters. </param>
+		public void ContextCallback(IAsyncResult result) {
+			try {
+				byte[] data = new byte[1000];
+				string requestStr;
+				int respLen = 0, valid;
+
+				// Call EndGetContext to complete the asynchronous operation.
+				HttpListenerContext context = m_Svr.EndGetContext(result);
+				HttpListenerRequest request = context.Request;
+
+				// Obtain a response object.
+				HttpListenerResponse response = context.Response;
+
+				// Construct a response.
+				string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+				byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+				// Search for image record
+				requestStr = m_ImgDir + "\\" + request.RawUrl.Substring(m_DomainSize);			// Note: Clunky but fast
+				if (File.Exists(requestStr)) {
+					using (BinaryReader reader = new BinaryReader(File.Open(requestStr, FileMode.Open))) {
+						while (respLen < reader.BaseStream.Length) {
+							valid = reader.Read(data, 0, data.Length);
+							response.OutputStream.Write(data, 0, valid);
+							respLen += valid;
+						}
+						response.OutputStream.Flush();
+						response.OutputStream.Close();
+					}
+				}
+
+				m_Svr.BeginGetContext(new AsyncCallback(ContextCallback), m_Svr);
+			}
+			catch (Exception ex) {
+				Log("Async Callback encountered an error. " + ex.Message);
+			}
+		}
+		/// <summary> Populate files listview with file names for user reference. Only called at startup. </summary>
+		public void DisplayFiles() {
+			string[] files = Directory.GetFiles(m_ImgDir);
+
+			LvwFiles.BeginUpdate();
+			LvwFiles.Items.Clear();
+			m_Images.Clear();
+			foreach (string file in files) {
+				string rawName = file.Substring(file.LastIndexOf("\\") + 1);
+				LvwFiles.Items.Add(new ListViewItem(rawName));
+				m_Images.Add(rawName);
+
+			}
+			LvwFiles.EndUpdate();
+		}
+
+		/// <summary> Run Server/Stop Server button </summary>
+		private void BtnRun_Click(object sender, EventArgs e) {
+			if (m_Svr == null || !m_Svr.IsListening) {
+				// Initialize and run server
+				if (m_Svr != null) {
+					m_Svr.Stop();
+					m_Svr.Close();
+				}
+				m_Svr = new HttpListener();
+				m_Svr.Prefixes.Clear();
+				m_Svr.Prefixes.Add(LblServerPrefix.Text);
+				m_Svr.Start();
+				m_Svr.BeginGetContext(new AsyncCallback(ContextCallback), m_Svr);
+
+				// Cosmetic
+				BtnRun.BackColor = Color.Yellow;
+				BtnRun.Text = "Stop Server";
+				TbxAddr.Enabled = TbxDomain.Enabled = TbxPort.Enabled = false;
+				m_DomainSize = TbxDomain.Text.Length + 2;
+
+				// Logging
+				Log("Server started with domain " + LblServerPrefix.Text);
+			} else {
+				m_Svr.Stop();
+
+				// Cosmetic
+				BtnRun.BackColor = Color.LightGreen;
+				BtnRun.Text = "Run Server";
+				TbxAddr.Enabled = TbxDomain.Enabled = TbxPort.Enabled = true;
+
+				// Logging
+				Log("Server stopped");
+			}
+		}
+		/// <summary> Update server resource string on text change. </summary>
+		private void ServerParam_TextChanged(object sender, EventArgs e) {
+			IPAddress addr;
+			int port;
+
+			if (TryParse(TbxAddr, out addr) && TryParse(TbxPort, out port)) {
+				LblServerPrefix.Text = "http://" + TbxAddr.Text + ":" + port.ToString() + "/" + TbxDomain.Text + "/";
+			}
+		}
+
+		/// <summary> Barebones logging. This is ugly and inefficient but the server should be low-demand due to it's local nature. </summary>
+		/// <param name="Text"> The string to be logged. </param>
+		private void Log(string Text) {
+			if (InvokeRequired) {
+				BeginInvoke(new MethodInvoker(delegate () { Log(Text); }));
+			} else {
+				string output = "";
+
+				Text = "[ " + DateTime.Now.ToString("hh:mm:ss") + " ]  " + Text + "\r\n";
+				m_Log.Write(Text.ToCharArray());
+
+				m_LBack.Enqueue(Text);
+				if (m_LBack.Count > LOG_MAX) { m_LBack.Dequeue(); }
+
+				foreach (string log in m_LBack) {
+					output += log;
+				}
+
+				TbxLog.Text = output;
+			}
+		}
+		/// <summary> Try to parse a value from a textbox. If parse fails, alert user. </summary>
+		/// <typeparam name="T"> The type of the data being parsed. </typeparam>
+		/// <param name="Tbx"> The textbox containing the string to be parsed. </param>
+		/// <param name="Obj"> The return value, if parsing is successful. </param>
+		/// <returns> True if the parse succeeded. Otherwise, returns false. </returns>
+		private bool TryParse<T>(TextBox Tbx, out T Obj) {
+			try {
+				Tbx.BackColor = Color.White;
+
+				if (typeof(T) == typeof(IPAddress)) {
+					IPAddress[] addrs = Dns.GetHostAddresses(Tbx.Text);
+
+					if (addrs.Length != 0) {
+						int ipv4Addr = -1;
+						for (int i = 0; i < addrs.Length; i++) {
+							if (addrs[i].AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) {
+								ipv4Addr = i;
+							}
+						}
+
+						if (ipv4Addr != -1) {
+							Obj = (T)Convert.ChangeType(addrs[ipv4Addr], typeof(T));
+						} else {
+							Obj = (T)Convert.ChangeType(IPAddress.Parse(Tbx.Text), typeof(T));
+						}
+					} else {
+						Obj = (T)Convert.ChangeType(IPAddress.Parse(Tbx.Text), typeof(T));
+					}
+				} else {
+					Obj = (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromString(null, CultureInfo.InvariantCulture, Tbx.Text);
+				}
+
+				return true;
+			}
+			catch {
+				Tbx.BackColor = Color.Red;
+			}
+			Obj = default;
+			return false;
 		}
 	}
 }
